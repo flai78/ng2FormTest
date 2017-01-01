6e664c17a8334b13718ff06bdfbe2360e2d948ee
flai78 <fililai@gmail.com>
2017-01-02 00:07:00 +0100

    new cng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { MyModel } from '../models/mymodel.model';
 
 @Component({
   selector: 'home',
@@ -6,4 +7,6 @@ import {Component} from '@angular/core';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  mySelectList = ['val1','val2','val3'];
+  model = new MyModel('Fili','Lai',true,'val1','val2');
 }
---
+++ src/app/models/mymodel.model.ts
@@ -0,0 +1,11 @@
+export class MyModel{
+    constructor(
+        public name: string,
+        public lastName: string,
+        public isChk: boolean,
+        public rdbVal: string,
+        public selectItem: string
+    ){
+        
+    }
+}
